85da9da3fd98560d597bf8b032bf4e99b4f372b4
Res0ution <maestro@hotger.com>
2025-01-26 23:24:47 +0300

    main header

--- app/components/header.js
+++
@@ -1,8 +0,0 @@
-export default function Header () {
-  return (
-    <>
-      <img src="/logo.png" alt="Logo image"></img>
-      <h1>Welcome to this project</h1>
-    </>
-  )
-}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,10 @@
 import styles from "./page.module.css";
 import Link from "next/link";
-import Header from "@/components/header";
 
 export default function Home() {
-  console.log('...Executing')
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Header />
         <p><Link href="/meals">Meals</Link></p>
         <p><Link href="/meals/share">Share Meals</Link></p>
         <p><Link href="/community">Community</Link></p>
