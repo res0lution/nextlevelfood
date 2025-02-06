d1315196d616024b891db2ef837842a189ddc1c8
Res0ution <maestro@hotger.com>
2025-02-06 22:24:12 +0300

    loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,16 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +26,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
